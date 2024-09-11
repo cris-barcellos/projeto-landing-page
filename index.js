3d01c607767c9410e49ed50e1ddca9fff126a3d5
cris-barcellos <78373829+cris-barcellos@users.noreply.github.com>
2024-09-11 19:26:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var Leonardo = window.document.getElementById(Leonardo)
+var Samantha = window.document.getElementById(Samantha)
+var Bruna = window.document.getElementById(Bruna)
+
+function RolarParaDireita(){
+    Leonardo.style ="display:none"
+    Bruna.style ="display:flex"
+}
+
+function RolarParaEsquerda()
